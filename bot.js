9a0e0b4492758c2236720ba80d1a670e1366e44a
BestCoderz <utk.bkkg@gmail.com>
2019-01-09 12:13:57 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,5 +43,26 @@ client.on('guildMemberAdd', member => {
   });
 });
 
+client.on('message', async msg => { // eslint-disable-line
+    if (msg.author.bot) return undefined;
+    if (!msg.content.startsWith(PREFIX)) return undefined;
+    const args = msg.content.split(' ');
+     
+    if(msg.content.startsWith(`${PREFIX}helpmusic`)){
+        var embedhelp = new Discord.RichEmbed()
+            .setTitle(`MultiverseMusic Commands`)
+            .addField("mv!play [YouTube Link/Playlist]", "Usage: `mv!play` Description: To play See The YouTube Linke And playlist.", false)
+            .addField("mv!play [Suchbegriff(e)]", "Usage: `mv!play`<song name> Description: To play Music.", false)
+            .addField("mv!skip", "Usage: `mv!skip` Description: To skip music.", false)
+            .addField("mv!stop", "Usage: `mv!stop` Description: To Bot disconnected.", false)
+            .addField("mv!song", "Usage: `mv!song` Description: To Check The Current playing song.", false)
+            .addField("mv!queue", "Usage: `mv!queue` Description: To Check The Queue List.", false)
+            .setColor([226, 50, 41])
+            .setThumbnail(client.user.avatarURL)
+            return msg.channel.sendEmbed(embedhelp);
+    }
+    return undefined;
+});
 
+ 
 client.login(process.env.BOT_TOKEN);
